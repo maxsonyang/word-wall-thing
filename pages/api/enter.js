f18dd4d0108af271ef8b0a5607f77f83d891a4f1
Maxson Yang <maxsonyang@berkeley.edu>
2023-03-26 11:13:49 -0700

    created an entry page

---
+++ pages/api/enter.js
@@ -0,0 +1,20 @@
+const Enter = (props) => {
+  return (
+    <main>
+    </main>
+  )
+}
+
+const SignInButton = () => {
+  return <></>
+}
+
+const SignOutButton = () => {
+  return <></>
+}
+
+const UserNameForm = () => {
+  return <></>;
+}
+
+export default Enter;
