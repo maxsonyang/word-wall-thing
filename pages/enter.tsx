424d4dda08a40d7da4640a4fcaa772538295b531
Maxson Yang <maxsonyang@berkeley.edu>
2023-03-26 15:37:30 -0700

    had a duplicate enter

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -1,11 +1,39 @@
-const EnterPage = ({ }) => {
-  return (
-    <main>
-      <h1>
-        Sign Up
-      </h1>
-    </main>
-  )
-}
+import { auth, googleAuthProvider } from '../lib/firebase'
 
-export default EnterPage;
+const Enter = ({ user, username }) => {
+
+  /**
+   * Shows components depending on three cases:
+   * 1. if user is signed out, show the sign-in button.
+   * 2. If the user is signed in but does not have a username, show username form
+   * 3. Otherwise show the signout button.
+   */
+  const getAuthProp = () => {
+    if (!user) return <SignInButton />
+    if (!username) return <UserNameForm />
+    return <SignOutButton />
+  };
+
+  return <main>
+    {getAuthProp()}
+  </main>;
+};
+
+const SignInButton = () => {
+
+  const signInWithGoogle = async () => {
+    await auth.signInWith
+  }
+
+  return <></>;
+};
+
+const SignOutButton = () => {
+  return <></>;
+};
+
+const UserNameForm = () => {
+  return <></>;
+};
+
+export default Enter;
