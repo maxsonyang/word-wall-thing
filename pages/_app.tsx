5a6954678212cf5823d31e3bc7b06e997e17c9ab
Maxson Yang <maxsonyang@berkeley.edu>
2023-03-26 22:31:05 -0700

    moved authentication state logic into a hook'

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,36 +2,17 @@
 import Navbar from '@/components/Navbar'
 import '@/styles/globals.scss'
 import { UserContext } from '@/lib/context';
-import { auth, firestore } from '@/lib/firebase';
+import { firestore } from '@/lib/firebase';
+import { useUserData } from '@/lib/hooks';
 
 // EXTERNAL LIBRARIES
 import type { AppProps } from 'next/app'
-import { useState, useEffect } from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
+
 import { Toaster } from 'react-hot-toast';
 
 export default function App({ Component, pageProps }: AppProps) {
 
-  const [user] = useAuthState(auth);
-  const [username, setUsername] = useState(null);
-
-  useEffect(() => {
-    let unsubscribe;
-
-    if (user) {
-      const ref = firestore.collection('users').doc(user.uid);
-      unsubscribe = ref.onSnapshot((doc) => {
-        let data = doc.data()
-        if (data) {
-          setUsername(data.username);
-        } else {
-          setUsername(null);
-        }
-      });
-    }
-
-    return unsubscribe;
-  }, [user])
+  const { user, username } = useUserData()
 
   // @ts-ignore
   return <UserContext.Provider value={{ user, username }}>
